9ff4963e66bb8ff170823f028180cce940df942e
Jie-666 <1661083959@qq.com>
2021-06-22 20:14:53 +0800

    day02完成项目搭建

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/mapper/SkuAttrValueMapper.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.pms.mapper;
+
+import com.atguigu.gmall.pms.entity.SkuAttrValueEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * sku销售属性&值
+ * 
+ * @author kjj
+ * @email kjj@atguigu.com
+ * @date 2021-06-22 18:47:27
+ */
+@Mapper
+public interface SkuAttrValueMapper extends BaseMapper<SkuAttrValueEntity> {
+	
+}
---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/service/AttrService.java
@@ -0,0 +1,21 @@
+package com.atguigu.gmall.pms.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.atguigu.gmall.common.bean.PageResultVo;
+import com.atguigu.gmall.common.bean.PageParamVo;
+import com.atguigu.gmall.pms.entity.AttrEntity;
+
+import java.util.Map;
+
+/**
+ * 商品属性
+ *
+ * @author kjj
+ * @email kjj@atguigu.com
+ * @date 2021-06-22 18:47:27
+ */
+public interface AttrService extends IService<AttrEntity> {
+
+    PageResultVo queryPage(PageParamVo paramVo);
+}
+
